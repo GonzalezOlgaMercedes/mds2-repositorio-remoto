4ea86c72903022bc1f589661a427687a6c748ed3
Gonzalez Olga Mercedes <olgamgonzalez97@gmail.com>
2025-08-15 20:49:53 -0300

    aquí vamos con el conflicto de ramas!

--- archivo.php
+++ archivo.php
@@ -1,2 +1,5 @@
 <?php
 echo "Soy un archivo muy bonito jeje";
+echo "Ahora estoy en la rama main";
+//Resulta ser que main y develop son ramas distintas que tienen las mismas líneas de código, así que aquí vamos con el conflicto de ramas!
+//Espero muy fuerte, a que esto funcione :v
