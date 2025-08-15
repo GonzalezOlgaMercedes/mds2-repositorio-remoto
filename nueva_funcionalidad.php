3a369344238ad05d4a20e29730efd1ba1082d018
Gonzalez Olga Mercedes <olgamgonzalez97@gmail.com>
2025-08-15 17:45:11 -0300

    feat(ui): agregar botón de registro.

---
+++ nueva_funcionalidad.php
@@ -0,0 +1,7 @@
+<?php
+echo "git checkout -b feature/nueva-funcionalidad develop";
+echo "Ahí develop al final indica desde qué rama quieres crear la nueva.
+En este caso: 
+feature/nueva-funcionalidad → nombre de la nueva rama.
+develop → rama base de la que se parte.
+Es útil cuando no quieres crear la nueva rama desde donde estás ahora, sino desde otra rama específica.";
